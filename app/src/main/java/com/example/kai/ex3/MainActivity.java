0cddbfc8625d3cea3b757b956d85e416774c0369
KaiOrland <kayorland@gmail.com>
2016-11-18 12:30:47 +0200

    ert

--- app/src/main/java/com/example/kai/ex3/MainActivity.java
+++ app/src/main/java/com/example/kai/ex3/MainActivity.java
@@ -2,22 +2,30 @@ package com.example.kai.ex3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener {
+    private double result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EditText ed1 = (EditText)findViewById(R.id.editText);
+        EditText ed2 = (EditText)findViewById(R.id.editText2);
+        ed1.addTextChangedListener(this);
+        ed2.addTextChangedListener(this);
+        Button equal = (Button)findViewById(R.id.button5);
+        equal.setOnClickListener(this);
     }
-    public void calculator(View view){
-        double result;
+
+    public void abc(View view){
         findViewById(R.id.editText);
         EditText ed1 = (EditText)findViewById(R.id.editText);
         String StrVar1 = ed1.getText().toString();
@@ -45,10 +53,48 @@ public class MainActivity extends AppCompatActivity {
                 result = 0;
                 break;
         }
-        if(view.getId()==R.id.button5){
-            TextView answer = (TextView) view.findViewById(R.id.textView4);
-            answer.setText(Double.toString(result));
+
         }
 
+
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+        EditText ed1 = (EditText)findViewById(R.id.editText);
+        EditText ed2 = (EditText)findViewById(R.id.editText2);
+        changeButtonState((!ed1.getText().toString().isEmpty())&&(!ed2.getText().toString().isEmpty()));
+
+    }
+    private void changeButtonState(boolean newState){
+        Button b1 = (Button)findViewById(R.id.button);
+        Button b2 = (Button)findViewById(R.id.button2);
+        Button b3 = (Button)findViewById(R.id.button3);
+        Button b4 = (Button)findViewById(R.id.button4);
+        Button b5 = (Button)findViewById(R.id.button5);
+        b1.setEnabled(newState);
+        b2.setEnabled(newState);
+        b3.setEnabled(newState);
+        b4.setEnabled(newState);
+        b5.setEnabled(newState);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button5) {
+            TextView answer = (TextView) v.findViewById(R.id.textView4);
+            answer.setText(Double.toString(result));
+        }
     }
 }
